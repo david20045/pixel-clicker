0883a2f06f562c3320ca4aa3611a62620c7bdb34
david20045 <david.ovanesyan.44@icloud.com>
2024-09-10 19:42:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Обработчик для корневого пути
+app.get('/', (req, res) => {
+    res.send('Сервер работает!');
+});
+
 let users = {}; // userId: {inviteCount, telegramSubscribed, invitedFriends, ...}
 let invites = {}; // inviteCode: userId
 
