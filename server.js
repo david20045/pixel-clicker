d6f285b159cfd20b8f2f089aecb0f1e7950cc6a8
david20045 <david.ovanesyan.44@icloud.com>
2024-09-10 14:17:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,32 @@
 const express = require('express');
-const cors = require('cors'); // Подключаем библиотеку CORS
-const fetch = require('node-fetch'); // Для запросов к Telegram API
+const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
-app.use(express.json()); // Позволяет серверу принимать JSON-данные
+app.use(express.json());
 
-// Настройка CORS для разрешения запросов с твоего фронтенд-домена
+// Настройка CORS для запросов с фронтенда
 const corsOptions = {
-    origin: 'https://david20045.github.io', // Разрешаем запросы с домена GitHub Pages
-    optionsSuccessStatus: 200 // Для старых браузеров
+    origin: 'https://david20045.github.io',
+    optionsSuccessStatus: 200
 };
 
-app.use(cors(corsOptions)); // Применяем CORS ко всем маршрутам
+app.use(cors(corsOptions));
 
-// Пример простого маршрута для проверки работы сервера
-app.get('/', (req, res) => {
-    res.send('Сервер работает!');
-});
-
-// Маршрут для генерации пригласительной ссылки
+// Генерация пригласительной ссылки
 app.post('/generate-invite', (req, res) => {
-    const userId = req.body.userId; // Получаем ID пользователя
-    const inviteCode = Date.now().toString(); // Генерируем уникальный код
-
-    // Формируем правильную ссылку для Telegram
+    const userId = req.body.userId;
+    const inviteCode = Date.now().toString();
     const inviteLink = `https://t.me/PixelClickerGameBot?start=${inviteCode}`;
-    res.json({ inviteLink }); // Возвращаем ссылку на клиент
+    res.json({ inviteLink });
 });
 
-// Маршрут для проверки подписки на Telegram
-const TELEGRAM_TOKEN = '7375840877:AAEiITuGMC44sMktZ6cl4qLFN6ZdkT_jBe4'; // Ваш токен
-const CHANNEL_ID = '@yourchannel'; // Замените на ID или название вашего Telegram-канала
+// Проверка подписки на Telegram канал
+const TELEGRAM_TOKEN = '7375840877:AAEiITuGMC44sMktZ6cl4qLFN6ZdkT_jBe4'; // Вставьте ваш токен
+const CHANNEL_ID = '@PixelClickerGameBot'; // ID вашего канала
 
 app.post('/check-subscription', async (req, res) => {
-    const userId = req.body.userId; // Получаем ID пользователя для проверки подписки
+    const userId = req.body.userId;
 
     try {
         const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/getChatMember?chat_id=${CHANNEL_ID}&user_id=${userId}`);
@@ -50,9 +43,8 @@ app.post('/check-subscription', async (req, res) => {
     }
 });
 
-// Используем динамический порт от Heroku или порт 3000 для локальной разработки
+// Запуск сервера
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
